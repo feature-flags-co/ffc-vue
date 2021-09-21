31d9e42ede781ecd9d01d11dce419e060f0e91d3
tao <yan.tao.life@gmail.com>
2021-09-21 18:05:26 +0200

    fix: fix export

--- example/main.js
+++ example/main.js
@@ -1,15 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import FFCPlugin from "../src/plugins";
+import FFCPlugin from "../src";
 
 // 初始化sdk，传入环境Secret Key和用户信息
 FFCPlugin.initialize({
   environmentSecret:
     "YThmLWRmZjUtNCUyMDIxMDkxNzA3NTYyMV9fMl9fMjJfXzExNl9fZGVmYXVsdF82NTM3Mg==",
 });
-
-FFCPlugin.initialize({ environmentSecret: "" });
 
 // 初始化用户信息，通常这一步会在登录后被调用
 FFCPlugin.initUserInfo({
